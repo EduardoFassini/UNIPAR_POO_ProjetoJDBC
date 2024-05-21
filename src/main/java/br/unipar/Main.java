e1424e31f8339e10411a533f842ccda91eda6fb1
EduardoFassini <e.fassini@edu.unipar.br>
2024-05-21 13:58:51 -0300

    A

--- src/main/java/br/unipar/Main.java
+++ src/main/java/br/unipar/Main.java
@@ -1,2 +1,45 @@
-package br.unipar;public class Main {
+package br.unipar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    private static final String url = "jdbc:postgresql://localhost:5432/exemplo1";
+    private static final String user = "postgres";
+    private static final String password = "admin123";
+
+    public static void main(String[] args) {
+        criarTabelaUsuario();
+    }
+
+    public static Connection connection() throws SQLException {
+
+        return DriverManager.getConnection(url,user,password);
+    }
+
+    public static void criarTabelaUsuario() {
+        try {
+            Connection conn = connection();
+
+            Statement statement = conn.createStatement();
+            String sql = " CREATE TABLE IF NOT EXISTS usuarios ( "
+                    + " codigo SERIAL PRIMARY KEY, "
+                    + " username VARCHAR(50) UNIQUE NOT NULL, "
+                    + " password VARCHAR(300) NOT NULL), "
+                    + " nome VARCHAR(50) NOT NULL), "
+                    + " nascimento DATE )";
+
+            statement.executeUpdate(sql);
+
+            System.out.println("Tabela criada com sucesso");
+
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+
 }
