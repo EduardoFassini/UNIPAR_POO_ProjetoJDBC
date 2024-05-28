00a980aea1d09eb537c6fb6ac7b75d2518efc4a5
EduardoFassini <e.fassini@edu.unipar.br>
2024-05-27 21:42:36 -0300

    A

--- src/main/java/br/unipar/Main.java
+++ src/main/java/br/unipar/Main.java
@@ -1,9 +1,6 @@
 package br.unipar;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -39,6 +36,47 @@ public class Main {
 
         } catch (SQLException exception) {
             exception.printStackTrace();
+        }
+    }
+
+    public static void InserirUsuario(String username, String password, String nome, String nascimento) {
+        try {
+            //Abre conexão
+            Connection conn = connection();
+
+            //Prepara a execução de um SQL
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "INSERT into usuario (username, password, nome, nascimento)"
+                            + "VALUES (?,?,?,?)"
+
+            );
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, nome);
+            preparedStatement.setDate(4, java.sql.Date.valueOf(nascimento));
+
+            preparedStatement.executeUpdate();
+
+            System.out.println("Usuário Inserido!");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void listarTodosUsuarios(){
+
+        try {
+            Connection conn = connection();
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM usuarios");
+            while(result.next()){
+                System.out.println(result.getInt("codigo"));
+                System.out.println(result.getString("username"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
